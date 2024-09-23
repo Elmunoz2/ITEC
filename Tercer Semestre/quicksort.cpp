70a9d4aaebfd3dd17dbe94169b766365326fd11e
Esteban Munoz <estebanmunoz42@gmail.com>
2024-09-23 16:22:00 -0600

    Actividad 6 - Algoritmo de Ordenamiento Quick Sort

---
+++ Tercer Semestre/quicksort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];  // Elegimos el último elemento como pivote
+    int i = low - 1;        // Índice del elemento más pequeño
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);  // Ordenamos los elementos a la izquierda del pivote
+        quickSort(arr, pi + 1, high); // Ordenamos los elementos a la derecha del pivote
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n - 1);
+    cout << "Array ordenado: ";
+    printArray(arr, n);
+    return 0;
+}
